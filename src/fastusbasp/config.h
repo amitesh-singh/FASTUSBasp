c1817db49b98d96efad405f17ce72f631de44097
Amitesh Singh <singh.amitesh@gmail.com>
2017-08-15 00:52:45 +0900

    create a header file for macro definitions

---
+++ src/fastusbasp/config.h
@@ -0,0 +1,12 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define ISP_PORT GPIOA
+#define ISP_RCC RCC_GPIOA
+
+#define ISP_MOSI GPIO7
+#define ISP_MISO GPIO6
+#define ISP_SCK  GPIO5
+#define ISP_RST GPIO8
+
+#endif
