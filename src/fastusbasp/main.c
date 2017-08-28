c54cb352043eeeeb7b48471219b3f62c9f646a92
Amitesh Singh <singh.amitesh@gmail.com>
2017-08-28 19:22:33 +0900

    Add support of hse8mhz_out_48Mhz clock
    Earlier we were using hsi_out_48Mhz which has
    clock tolerance upto 1% which is higher than usb clock tolerance
    12.0+-0.03 Mb/s ~ 0.25
    Although i did not face any problems yet.
    Also HSI is temperature sensitive so its better to use HSE instead.
    libopencm3 does not have this function so write one and add it.
    
    This needs to be tested one time

--- src/fastusbasp/main.c
+++ src/fastusbasp/main.c
@@ -23,6 +23,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 #include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/flash.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,14 +34,77 @@
 #endif
 
 #include "sleep.h"
-
 #include "usbdevice.h"
+
+extern uint32_t rcc_ahb_frequency;
+extern uint32_t rcc_apb1_frequency;
+
+void rcc_clock_setup_in_hse_8mhz_out_48mhz(void)
+{
+   /* Enable internal high-speed oscillator. */
+   rcc_osc_on(RCC_HSI);
+   rcc_wait_for_osc_ready(RCC_HSI);
+
+   /* Select HSI as SYSCLK source. */
+   rcc_set_sysclk_source(RCC_CFGR_SW_SYSCLKSEL_HSICLK);
+
+   /* Enable external high-speed oscillator 8MHz. */
+   rcc_osc_on(RCC_HSE);
+   rcc_wait_for_osc_ready(RCC_HSE);
+   rcc_set_sysclk_source(RCC_CFGR_SW_SYSCLKSEL_HSECLK);
+
+   /*
+    * Set prescalers for AHB, ADC, ABP1, ABP2.
+    * Do this before touching the PLL (TODO: why?).
+    */
+   rcc_set_hpre(RCC_CFGR_HPRE_SYSCLK_NODIV);    /* Set. 24MHz Max. 72MHz */
+   rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV2);  /* Set. 12MHz Max. 14MHz */
+   rcc_set_ppre1(RCC_CFGR_PPRE1_HCLK_NODIV);    /* Set. 24MHz Max. 36MHz */
+   rcc_set_ppre2(RCC_CFGR_PPRE2_HCLK_NODIV);    /* Set. 24MHz Max. 72MHz */
+
+   /*
+    * Sysclk runs with 24MHz -> 0 waitstates.
+    * 0WS from 0-24MHz
+    * 1WS from 24-48MHz
+    * 2WS from 48-72MHz
+    */
+   flash_set_ws(FLASH_ACR_LATENCY_1WS);
+
+   /*
+    * Set the PLL multiplication factor to 3.
+    * 8MHz (external) * 6 (multiplier) = 48MHz
+    */
+   rcc_set_pll_multiplication_factor(RCC_CFGR_PLLMUL_PLL_CLK_MUL6);
+
+   /* Select HSE as PLL source. */
+   rcc_set_pll_source(RCC_CFGR_PLLSRC_HSE_CLK);
+
+   /*
+	 * External frequency undivided before entering PLL
+	 * (only valid/needed for HSE).
+	 */
+   rcc_set_pllxtpre(RCC_CFGR_PLLXTPRE_HSE_CLK);
+
+   /* Enable PLL oscillator and wait for it to stabilize. */
+   rcc_osc_on(RCC_PLL);
+   rcc_wait_for_osc_ready(RCC_PLL);
+
+   /* Select PLL as SYSCLK source. */
+   rcc_set_sysclk_source(RCC_CFGR_SW_SYSCLKSEL_PLLCLK);
+
+   /* Set the peripheral clock frequencies used */
+   rcc_ahb_frequency = 48000000;
+   rcc_apb1_frequency = 24000000; //TODO: try with 36MHz since apb1 can have speed upto 36Mhz at max
+   rcc_apb2_frequency = 48000000;
+}
 
 int
 main()
 {
-   //set STM32 to 48 MHz
-   rcc_clock_setup_in_hsi_out_48mhz();
+   //rcc_clock_setup_in_hsi_out_48mhz();
+   //set STM32 to 48 MHz using ext. clock
+   rcc_clock_setup_in_hse_8mhz_out_48mhz();
+
    inbuilt_led_init();
 
 #ifdef DEBUG
