a56d879dc8392abcec16bd3562d6f8aecd8b04de
Amitesh Singh <singh.amitesh@gmail.com>
2023-12-21 18:49:29 +0530

    Add workflow and add libopencm3 as submodule

--- config.cmake
+++ config.cmake
@@ -1,5 +1,5 @@
 # Always declare this before stm32-toolchain.cmake
 #
 set(MCU STM32F1)
-set(LIBOPENCM3_DIR "/home/ami/repos/libopencm3")
+set(LIBOPENCM3_DIR "./libopencm3")
 set(SERIAL_PORT "/dev/ttyUSB0")
